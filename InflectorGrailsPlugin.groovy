558360ae1ede3358eaf46b28f113af43fc2cae28
James Cook <james.cookie@gmail.com>
2012-11-04 22:45:47 +0000

    Make plugin available for 1.3.7

--- InflectorGrailsPlugin.groovy
+++ InflectorGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class InflectorGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
